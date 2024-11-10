d00a88c113b1bf7063993b0f70d0a36a8606cc7d
Mohamed Zanana <mzanana@c3r5p10.1337.ma>
2024-11-10 21:02:20 +0100

    edit all files

--- Libft/libft.h
+++ Libft/libft.h
@@ -1,11 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 12:31:43 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/10 12:34:04 by mzanana          ###   ########.fr       */
+/*   Created: 2024/11/10 12:40:13 by mzanana           #+#    #+#             */
+/*   Updated: 2024/11/10 12:44:06 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+size_t				strlen(const char *s);
+size_t				ft_strlen(const char *s);
+void				*ft_memset(void *s, int c, size_t n);
+#endif

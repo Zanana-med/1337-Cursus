c60be6523598401e6ba96a999ff5afcad7eea049
Mohamed Zanana <mzanana@c3r5p10.1337.ma>
2024-11-11 01:53:19 +0100

    memset

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,33 +6,20 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 08:05:27 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/10 12:00:52 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/11 01:52:51 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
+
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t x = 0;
-	unsigned char *str = (unsigned char *)(s);
+	size_t			x;
+	unsigned char	*str;
 
+	str = (unsigned char *)(s);
+	x = 0;
 	while (x < n)
 		str[x++] = (unsigned char)(c);
-	return s;
-
-}
-
-int	main()
-{
-	int str = 0;
-	char *ptr = (char *)(&str);
-
-	ft_memset(ptr, 57, 1);
-	printf("57     ???  -> %d\n", str);
-	memset(ptr + 1, 5, 1);
-	printf("1337   ???  -> %d\n", str);
-	memset(ptr + 2, 4, 1);
-	printf("263481 ???  -> %d\n", str);
-
+	return (s);
 }

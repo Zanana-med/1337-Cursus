f5f692567eebab94955a312b2ad357fda5d65712
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-18 21:20:10 +0100

    Norminette Update

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,20 +6,19 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 18:12:11 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 21:08:53 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/18 21:18:58 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
-int	num_count(int n)
+static int	num_count(int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (n < 0)
 		i = 1;
-	while(n)
+	while (n)
 	{
 		n /= 10;
 		i++;
@@ -30,15 +29,15 @@ int	num_count(int n)
 char	*ft_itoa(int n)
 {
 	char	*ret;
-	int	count;
+	int		count;
 
-	if(n == 0)
-		return(ft_strdup("0"));
-	if(n == -2147483648)
-		return(ft_strdup("-2147483648"));
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	count = num_count(n);
 	ret = malloc(sizeof(char) * (count + 1));
-	if(!ret)
+	if (!ret)
 		return (NULL);
 	if (n < 0)
 	{
@@ -46,32 +45,10 @@ char	*ft_itoa(int n)
 		ret[0] = '-';
 	}
 	ret[count] = '\0';
-	while(n)
+	while (n)
 	{
 		ret[--count] = n % 10 + 48;
 		n /= 10;
 	}
 	return (ret);
 }
-/*
-int	main()
-{
-	for(int i = -2147483648; i < 0; i /= 2)
-		{
-			printf("%d\t", i);
-			char *s = ft_itoa(i);
-			printf("%s\n", s );
-			free(s);
-		}
-	for(int i = 2147483647; i > 0; i /= 2)
-		{
-			printf("%d\t", i);
-			char *s = ft_itoa(i);
-			printf("%s\n",  s);
-			free(s);
-		}
-	printf("%d\t", 0);
-	char *s = ft_itoa(0);
-			printf("%s\n", s );
-			free(s);
-}*/

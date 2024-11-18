6aab899a6b9b6dc48d658cd691eb147648b7a7c2
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-19 00:14:36 +0100

    Using Pool_Putnbr Function

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 23:42:26 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/19 00:00:17 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/19 00:14:25 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,9 +17,14 @@ void	ft_putnbr_fd(int n, int fd)
 
 	if (fd < 0)
 		return ;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
-		n *= -1;
+		n = -n;
 		write (fd, "-", 1);
 	}
 	if (n > 9)

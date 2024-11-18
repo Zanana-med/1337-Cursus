c5d49b9e3cca57830fce5fcd47a4ac1ba76741b5
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-18 23:40:05 +0100

    putend update

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 23:33:59 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 23:36:45 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/18 23:39:40 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,5 +16,5 @@ void	ft_putendl_fd(char *s, int fd)
 	if (!s)
 		return ;
 	write (fd, s, ft_strlen(s));
-	write (fd, "\n", 2);
+	write (fd, "\n", 1);
 }

54c0c8eafc285ab926f6d52fcfa8dc06b826b342
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-18 23:37:11 +0100

    Putend_fd Update

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 12:04:56 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 23:29:14 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/18 23:33:44 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -49,4 +49,5 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void		ft_striteri(char *s, void (*f)(unsigned int, char*));
 void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
 #endif

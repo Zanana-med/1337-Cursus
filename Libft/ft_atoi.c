42f753ebd807adbb1e513b2cf81b641bf2585068
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-18 16:47:13 +0100

    Atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,16 +6,16 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 09:00:00 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 16:30:17 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/18 16:46:49 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <limits.h>
+#include <stdio.h>
 
 static void	space_check(const char *s, int *index)
 {
-	while (s[*index] && ((s[*index] >= 9 && s[*index] <= 13
-				|| s[*index] == 32)))
+	while ((s[*index]) && (((s[*index] >= 9) && (s[*index] <= 13
+				|| s[*index] == 32))))
 		*index += 1;
 }
 
@@ -39,12 +39,7 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	res = 0;
 	space_check(str, &index);
-	/*if (str[index] == '+' || str[index] == '-')
-	{
-		if (str[index] == '-')
-			sign = -1;
-		index++;
-	}*/
+	sign_check(str[index], &index, &sign);
 	while (str[index] && (str[index] >= 48 && str[index] <= 57))
 	{
 		if (res > LLONG_MAX)
@@ -58,3 +53,8 @@ int	ft_atoi(const char *str)
 	}
 	return (res * sign);
 }
+/*
+int main()
+{
+	printf("-254 : %d | +200 : %d | --215(0) : %d\n", ft_atoi("   -254.36"), ft_atoi("      +200[2500"), ft_atoi(" --215"));
+}*/

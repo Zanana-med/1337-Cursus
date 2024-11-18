867641f638d4087f48b07f6477977a044b7e8f12
Mohamed Zanana <mzanana@c3r3p3.1337.ma>
2024-11-18 16:30:43 +0100

    ATOI

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 09:00:00 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 12:03:43 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/18 16:30:17 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,9 +14,19 @@
 
 static void	space_check(const char *s, int *index)
 {
-	while (s[*index] && ((s[*index] >= 9 && s[*index] <= 13)
-                        || s[*index] == 32))
+	while (s[*index] && ((s[*index] >= 9 && s[*index] <= 13
+				|| s[*index] == 32)))
 		*index += 1;
+}
+
+static void	sign_check(const char c, int *index, int *sign)
+{
+	if (c == '+' || c == '-')
+        {
+                if (c == '-')
+                        *sign = -1;
+                *index += 1;
+        }
 }
 
 int	ft_atoi(const char *str)
@@ -28,16 +38,13 @@ int	ft_atoi(const char *str)
 	index = 0;
 	sign = 1;
 	res = 0;
-	//while (str[index] && ((str[index] >= 9 && str[index] <= 13)
-	//		|| str[index] == 32))
-	//	index++;
 	space_check(str, &index);
-	if (str[index] == '+' || str[index] == '-')
+	/*if (str[index] == '+' || str[index] == '-')
 	{
 		if (str[index] == '-')
 			sign = -1;
 		index++;
-	}
+	}*/
 	while (str[index] && (str[index] >= 48 && str[index] <= 57))
 	{
 		if (res > LLONG_MAX)

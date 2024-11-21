11d11f67a1eb8441142bcc44310c102f9f108ebd
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 00:07:53 +0100

    the file lstlast

--- Libft/ft_lstlast_bonus.c
+++ Libft/ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 23:58:37 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 00:06:31 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 00:07:49 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	ret;
 
 	ret = lst;
-	while (ret)
-		ret = ret -> ret;
-	return (lst);
+	while (ret -> next)
+		ret = ret -> next;
+	return (ret);
 }

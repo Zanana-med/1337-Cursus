0ea910c9805837271382579f1faa920ed1faf893
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-21 17:49:37 +0100

    fix the malloc fail return

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 17:31:06 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/21 17:34:00 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/21 17:49:14 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,6 +16,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
 	new -> content = content;
 	new -> next = NULL;
 	return (new);

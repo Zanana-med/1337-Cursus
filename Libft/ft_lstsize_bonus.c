eb24e83e659351ceabbccc9985e6cdb06189fff5
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-21 22:17:56 +0100

    first try

---
+++ Libft/ft_lstsize_bonus.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 21:09:39 by mzanana           #+#    #+#             */
+/*   Updated: 2024/11/21 22:17:13 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+//#include <stdio.h>
+int ft_lstsize(t_list *lst)
+{
+	int	ret;
+	
+	ret = 0;
+	while (lst)
+	{
+		ret++;
+		lst = lst -> next;
+	}
+	return (ret);
+}
+/*
+int	main()
+{
+	t_list *head, *current, *go;
+
+	head = ft_lstnew((char*) "Hello\n");
+	current = ft_lstnew((char*) "HELLO\n");
+	head -> next = current;
+	current = ft_lstnew((char*) "HELLO\n");
+	head -> next -> next = current;
+	go = head;
+	while(go)
+	{
+		printf("%s", (char *)(go -> content));
+		go = go -> next;
+	}
+	printf("\n%d\n",ft_lstsize(head));
+
+
+}*/

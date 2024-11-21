7aba10fa5de314e056273cb79de850462d5a8334
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 00:37:45 +0100

    List Add Back creation

--- Libft/ft_lstadd_back_bonus.c
+++ Libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 00:15:49 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 00:34:17 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 00:37:33 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -26,5 +26,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	while (current && current -> next)
 		current = current -> next;
 	current -> next = new;
-	new -> next = NULL;
 }

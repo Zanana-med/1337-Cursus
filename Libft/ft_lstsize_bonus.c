dd1bfaa784ec3ce21e1b346a9b4f175525ddf4e5
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-21 22:39:22 +0100

    handle null

--- Libft/ft_lstsize_bonus.c
+++ Libft/ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 21:09:39 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/21 22:21:45 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/21 22:39:06 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,6 +15,8 @@ int ft_lstsize(t_list *lst)
 {
 	int	ret;
 	
+	if (!lst)
+		return (0);
 	ret = 0;
 	while (lst)
 	{

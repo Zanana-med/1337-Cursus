de5400a63c79d6992210eeb8eda728daba9c6c4c
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-21 17:34:33 +0100

    creation of the lstnew

---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 17:31:06 by mzanana           #+#    #+#             */
+/*   Updated: 2024/11/21 17:34:00 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}

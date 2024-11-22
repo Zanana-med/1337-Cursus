67355d1adbabfbf70e199b4e1aa0452d34924cd0
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 01:57:28 +0100

    lets check again

--- Libft/ft_lstclear_bonus.c
+++ Libft/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 01:41:27 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 01:56:55 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 01:57:24 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,7 +22,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	current = *lst;
 	while (current)
 	{
-		ft_lstdelone(current, del(current -> content));
+		ft_lstdelone(current, del(current, current -> content));
 		current = current -> next;
 	}
 	free (lst);

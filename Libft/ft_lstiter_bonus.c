ad6134501b4dfcac8f8276cf0eadbdd05ffa9422
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 02:22:49 +0100

    Norminette Update

--- Libft/ft_lstiter_bonus.c
+++ Libft/ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 02:13:00 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 02:21:30 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 02:22:33 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,6 +14,7 @@
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	t_list	*current;
+
 	if (!lst || !f)
 		return ;
 	current = lst;

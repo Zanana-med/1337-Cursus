634c02ba9a18ca5ae4a8edae29f009087bf34bca
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 02:48:48 +0100

    lstmap added

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 12:04:56 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 02:13:45 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 02:47:28 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -65,5 +65,5 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif

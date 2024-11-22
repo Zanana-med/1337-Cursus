0665bf860adc4037ec1a4f3bc86d86e52af82773
Mohamed Zanana <mzanana@c3r2p5.1337.ma>
2024-11-22 02:21:39 +0100

    Iterate LinkedList

--- Libft/ft_lstiter_bonus.c
+++ Libft/ft_lstiter_bonus.c
@@ -6,18 +6,20 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 02:13:00 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/22 02:16:11 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/22 02:21:30 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*current;
 	if (!lst || !f)
 		return ;
-	while (lst)
+	current = lst;
+	while (current)
 	{
-		f(lst -> content);
-		lst = lst -> next;
+		f(current -> content);
+		current = current -> next;
 	}
 }

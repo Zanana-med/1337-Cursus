99f142e2d46bf357e9e19b26ea8faddde67bbdbd
Mohamed Zanana <mr.zananamohamed@gmail.com>
2024-11-26 01:29:19 +0100

    useless casting to const

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,21 +6,21 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 14:09:02 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/17 23:22:09 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/26 01:28:50 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*str1;
-	const unsigned char	*str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 	size_t				index;
 
 	if (n == 0 || (!s1 && !s2))
 		return (0);
-	str1 = (const unsigned char *)s1;
-	str2 = (const unsigned char *)s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	index = 0;
 	while ((*(str1 + index) == *(str2 + index)) && index < (n - 1))
 		index++;

9e27d95f9132f81a0cf53fcaf43cccbe49a7226d
Mohamed Zanana <mr.zananamohamed@gmail.com>
2024-11-26 01:28:15 +0100

    don't need to cast to const

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,18 +6,18 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 12:56:24 by mzanana           #+#    #+#             */
-/*   Updated: 2024/11/18 17:38:13 by mzanana          ###   ########.fr       */
+/*   Updated: 2024/11/26 01:27:58 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*str;
+	unsigned char	*str;
 	unsigned char		ch;
 	size_t				index;
 
-	str = (const unsigned char *)s;
+	str = (unsigned char *)s;
 	ch = (unsigned char)c;
 	index = 0;
 	while (index < n)
